3253b8c12e4fba60d9a41f3820337311507b329a
Punit <punitmistr@gmail.com>
2024-06-05 16:41:33 +0530

    changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Header from "./components/header";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState({
+    first: "",
+    second: "",
+  });
 
+  const initialTodos = [
+    {
+      id: 1,
+      title: "Todo 1",
+      complete: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      complete: false,
+    },
+  ];
+  const callApi = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    const data = await response.json();
+    console.log(data);
+    setCount({ ...count, first: data.title });
+  };
+  useEffect(() => {
+    callApi();
+  }, []);
+  const changeFunction = (e: any) => {
+    // console.log(e.target.value);
+    const value = e.target.value;
+    const name = e.target.name;
+    setCount((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header title="Todo List" />
+      <input
+        type="text"
+        name="first"
+        value={count.first}
+        onChange={changeFunction}
+      />
+      <input
+        type="text"
+        name="second"
+        value={count.second}
+        onChange={changeFunction}
+      />
+      <button onClick={() => console.log(count)}>Log</button>
+      {initialTodos.map((todo) => (
+        <div key={todo.id}>
+          <label>{todo.title}</label>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
